be47ccb4661c14e38c0449f328807b7b23c2201e
mohammedghallydev <mohammed.ghally110@gmail.com>
2024-02-17 00:52:14 +0200

    Final Commit 🚀

---
+++ src/features/user/userSlice.js
@@ -0,0 +1,69 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getAddress } from "../../services/apiGeocoding";
+
+function getPosition() {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+}
+
+export const fetchAddress = createAsyncThunk(
+  "user/fetchAddress",
+  async function () {
+    // 1) We get the user's geolocation position
+    const positionObj = await getPosition();
+    const position = {
+      latitude: positionObj.coords.latitude,
+      longitude: positionObj.coords.longitude,
+    };
+
+    // 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
+    const addressObj = await getAddress(position);
+    const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
+
+    // 3) Then we return an object with the data that we are interested in
+    // Payload of the FULFILLED state
+    return { position, address };
+  }
+);
+
+const initialState = {
+  username: "",
+  status: "idle",
+  position: {},
+  address: "",
+  error: "",
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    updateName(state, action) {
+      state.username = action.payload;
+    },
+  },
+  extraReducers: (builder) =>
+    builder.addCase(fetchAddress.pending, (state, action) => {
+      state.status = "loading";
+    })
+      .addCase(fetchAddress.fulfilled, (state, action) => {
+        state.position = action.payload.position;
+        state.address = action.payload.address;
+        state.status = "idle";
+      })
+      .addCase(fetchAddress.rejected, (state, action) => {
+        state.status = "error";
+        state.error = "There was a problem getting your address. Make sure to fill this field!";
+      })
+});
+
+export const { updateName } = userSlice.actions;
+
+export default userSlice.reducer;
+
+export const getUserName = (state) => state.user.username;
+
+/* 
+So, in the context of the updateName reducer, state is specifically referring to the state of the "user" slice. This is different from the overall Redux store state, which might include other slices if you have multiple slices in your store. Each slice has its own state, and the updateName reducer is responsible for updating the state of the "user" slice.
+*/
